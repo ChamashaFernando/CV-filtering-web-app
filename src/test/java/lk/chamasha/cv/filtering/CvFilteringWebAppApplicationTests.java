e9e2b38f42eb2eac88c1e19159f010722aed5fb1
ChamashaFernando <chamashanethmi81@gmail.com>
2025-05-15 11:35:38 +0530

    create model

---
+++ src/test/java/lk/chamasha/cv/filtering/CvFilteringWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.chamasha.cv.filtering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvFilteringWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
